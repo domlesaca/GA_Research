0c08a33cdcbd8ad6c45964ee70b6d3d006f9a4ff
domle <domlesaca@gmail.com>
2017-11-07 11:33:58 -0800

    added parameters to ReviewScrapper changed default link to go to review only page in trip advisor

--- Twitter_Parser/ReviewScrapper.py
+++ Twitter_Parser/ReviewScrapper.py
@@ -1,8 +1,28 @@
 from bs4 import BeautifulSoup
 import urllib.request
+from selenium import webdriver
+
+# Input the url for the specific page you want to scrape
+url = "https://www.tripadvisor.com/Hotel_Review-g45963-d91703-Reviews-or5-Bellagio_Las_Vegas-Las_Vegas_Nevada.html"
+# Input the name of the hotel you want to search
+hotelName = "Bellagio"
+# Input one if you want to scrape the URL or 0 if you want to scrape a search for a hotel
+scrapeType = 1
+#
+
+
 
 def main():
-    url = "https://www.tripadvisor.com/Hotel_Review-g45963-d91703-Reviews-Bellagio_Las_Vegas-Las_Vegas_Nevada.html"
+    # decide whether to scrape 1 url or do a search
+    if scrapeType == 1:
+        urlScrape()
+    else:
+        searchHotel()
+
+def searchHotel():
+    return
+
+def urlScrape():
     file = urllib.request.urlopen(url)
     soup = BeautifulSoup(file, "html.parser")
     # open file
@@ -21,6 +41,13 @@ def main():
         print(x)
 
     #print(soup.prettify())
+
+# def screenshot():
+#     DRIVER = webdriver.Chrome('C:\Program Files\Python36\selenium\webdriver\chromedriver')
+#     driver = webdriver.Chrome(DRIVER)
+#     driver.get(url)
+#     screenshot = driver.save_screenshot('my_screenshot.png')
+#     driver.quit()
 
 if __name__ == '__main__':
     main()
